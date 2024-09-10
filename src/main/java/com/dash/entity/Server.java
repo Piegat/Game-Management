c3470f4e698e966498bf7e177e65cb0fa4b529db
gustavo <gustavo.pgs@pti.org.br>
2024-09-10 08:37:08 -0300

    Update entity Server

--- src/main/java/com/dash/entity/Server.java
+++ src/main/java/com/dash/entity/Server.java
@@ -4,8 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class Server {
 
     @Id
