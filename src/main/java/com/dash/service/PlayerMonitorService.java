0b78469f3811e60c4cd66a1f2f857dcfcc458faf
gustavo <gustavo.pgs@pti.org.br>
2024-09-10 09:43:00 -0300

    Update PlayerMonitorService

--- src/main/java/com/dash/service/PlayerMonitorService.java
+++ src/main/java/com/dash/service/PlayerMonitorService.java
@@ -1,7 +1,9 @@
 package com.dash.service;
 
 import com.dash.entity.Player;
+import com.dash.entity.Server;
 import com.dash.repository.PlayerRepository;
+import com.dash.repository.ServerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -19,66 +21,75 @@ public class PlayerMonitorService {
     @Autowired
     private PlayerRepository playerRepository;
 
+    @Autowired
+    private ServerRepository serverRepository;
+
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String url = "https://servers-frontend.fivem.net/api/servers/single/";
+    private final String baseUrl = "https://servers-frontend.fivem.net/api/servers/single/";
 
     private final Map<String, LocalDateTime> playerStartTimes = new HashMap<>();
     private final Map<String, Boolean> playerStatus = new HashMap<>();
 
-    @Scheduled(fixedRate = 30000)
+    @Scheduled(fixedRate = 15000)
     public void checkPlayerStatus() {
-        try {
-            JsonNode response = restTemplate.getForObject(url, JsonNode.class);
+        List<Server> servers = serverRepository.findAll();
 
-            JsonNode playersList = response.path("Data").path("players");
+        for(Server server : servers) {
+            try {
+                String url = baseUrl + server.getIp();
 
-            List<Player> playerList = playerRepository.findAll();
+                JsonNode response = restTemplate.getForObject(url, JsonNode.class);
 
-            for (Player dbPlayer : playerList) {
-                String playerLicense = dbPlayer.getLicense();
-                boolean isPlayerOnline = false;
+                JsonNode playersList = response.path("Data").path("players");
 
-                for (JsonNode player : playersList) {
-                    for (JsonNode identifier : player.path("identifiers")) {
-                        if (identifier.asText().startsWith("license:") && identifier.asText().equals("license:" + playerLicense)) {
-                            isPlayerOnline = true;
+                List<Player> playerList = playerRepository.findAll();
+
+                for (Player dbPlayer : playerList) {
+                    String playerLicense = dbPlayer.getLicense();
+                    boolean isPlayerOnline = false;
+
+                    for (JsonNode player : playersList) {
+                        for (JsonNode identifier : player.path("identifiers")) {
+                            if (identifier.asText().startsWith("license:") && identifier.asText().equals("license:" + playerLicense)) {
+                                isPlayerOnline = true;
+                                break;
+                            }
+                        }
+                        if (isPlayerOnline) {
                             break;
                         }
                     }
-                    if (isPlayerOnline) {
-                        break;
+
+                    if (isPlayerOnline && (playerStatus.getOrDefault(playerLicense, false) == false)) {
+                        playerStatus.put(playerLicense, true);
+                        playerStartTimes.put(playerLicense, LocalDateTime.now());
+                        System.out.println("O jogador " + dbPlayer.getName() + " com license " + playerLicense + " está online  no servidor:" + server.getName() + ". Início marcado.");
+                    }
+                    else if (isPlayerOnline && playerStatus.getOrDefault(playerLicense, false)) {
+                        Duration elapsedTime = Duration.between(playerStartTimes.get(playerLicense), LocalDateTime.now());
+                        System.out.println("O jogador " + dbPlayer.getName() + " com license " + playerLicense + " está online há " + elapsedTime.toMinutes() + " minutos no servidor: " + server.getName());
+                        Long time_played;
+                        if(dbPlayer.getMinutes_played() == null){
+                            time_played = elapsedTime.toMinutes();
+                        }else {
+                            time_played = dbPlayer.getMinutes_played() + elapsedTime.toMinutes();
+                        }
+                        dbPlayer.setMinutes_played(time_played);
+                        playerRepository.save(dbPlayer);
+                    }
+                    else if (!isPlayerOnline && playerStatus.getOrDefault(playerLicense, false)) {
+                        playerStatus.put(playerLicense, false);
+                        Duration elapsedTime = Duration.between(playerStartTimes.get(playerLicense), LocalDateTime.now());
+                        System.out.println("O jogador " + dbPlayer.getName() + " com license " + playerLicense + " ficou offline. Estava online por " + elapsedTime.toMinutes() + " minutos  no servidor: " + server.getName());
+                        playerStartTimes.remove(playerLicense);
+                    } else {
+                        System.out.println("O jogador " + dbPlayer.getName() + "  com license " + playerLicense + " está offline no servidor: " + server.getName());
                     }
                 }
 
-                if (isPlayerOnline && (playerStatus.getOrDefault(playerLicense, false) == false)) {
-                    playerStatus.put(playerLicense, true);
-                    playerStartTimes.put(playerLicense, LocalDateTime.now());
-                    System.out.println("O jogador com license " + playerLicense + " está online. Início marcado.");
-                }
-                else if (isPlayerOnline && playerStatus.getOrDefault(playerLicense, false)) {
-                    Duration elapsedTime = Duration.between(playerStartTimes.get(playerLicense), LocalDateTime.now());
-                    System.out.println("O jogador com license " + playerLicense + " está online há " + elapsedTime.toMinutes() + " minutos.");
-                    Long time_played;
-                    if(dbPlayer.getMinutes_played() == null){
-                        time_played = elapsedTime.toMinutes();
-                    }else {
-                        time_played = dbPlayer.getMinutes_played() + elapsedTime.toMinutes();
-                    }
-                    dbPlayer.setMinutes_played(time_played);
-                    playerRepository.save(dbPlayer);
-                }
-                else if (!isPlayerOnline && playerStatus.getOrDefault(playerLicense, false)) {
-                    playerStatus.put(playerLicense, false);
-                    Duration elapsedTime = Duration.between(playerStartTimes.get(playerLicense), LocalDateTime.now());
-                    System.out.println("O jogador com license " + playerLicense + " ficou offline. Estava online por " + elapsedTime.toMinutes() + " minutos.");
-                    playerStartTimes.remove(playerLicense);
-                } else {
-                    System.out.println("O jogador com license " + playerLicense + " está offline.");
-                }
+            } catch (Exception e) {
+                System.out.println("Erro ao fazer requisição: " + e.getMessage());
             }
-
-        } catch (Exception e) {
-            System.out.println("Erro ao fazer requisição: " + e.getMessage());
         }
     }
 }
