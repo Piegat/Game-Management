dea544aa5d2c52afea56ac4c37b3b1f02de0963d
gustavo <gustavo.pgs@pti.org.br>
2024-09-10 08:29:54 -0300

    Create server repository

---
+++ src/main/java/com/dash/repository/ServerRepository.java
@@ -0,0 +1,10 @@
+package com.dash.repository;
+
+
+import com.dash.entity.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+
+}
+
